8f369d28f40100db1d4b32310a8628abbdc1ac88
widmarks <165108380+widmarks@users.noreply.github.com>
2024-05-03 23:43:18 +0200

    Add ui to rock paper scissors game

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const computerSelection = function () {
   let num = Math.floor(Math.random() * 3 + 1);
   return num === 1 ? "Rock" : num === 2 ? "Paper" : "Scissors";
 };
-const playerSelection = function () {
-  return prompt(`Rock Paper Scissors?`);
-};
 
 let playerScore = 0;
 let computerScore = 0;
@@ -28,21 +25,31 @@ function playRound(playerSelection, computerSelection) {
     return `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 }
+const div = document.querySelector(`.result`);
+div.textContent = `Your score: ${playerScore} --- Computer score: ${computerScore}`;
 
-function playGame() {
-  for (let i = 1; i <= 5; i++) {
-    console.log(
-      `Game ${[i]}: ${playRound(playerSelection(), computerSelection())}`
-    );
+const buttons = document.querySelectorAll(`button`);
+buttons.forEach((button) =>
+  button.addEventListener(`click`, () => {
+    const playerSelection = button.textContent;
+    let pick = playRound(playerSelection, computerSelection());
+    updateScore(pick);
+  })
+);
+
+const p = document.createElement(`p`);
+
+function updateScore(pick) {
+  div.textContent = `Your score: ${playerScore} --- Computer score: ${computerScore}`;
+  div.appendChild(p);
+  p.textContent = pick;
+  if (playerScore === 5) {
+    div.textContent = `You win!`;
+    playerScore = 0;
+    computerScore = 0;
+  } else if (computerScore === 5) {
+    div.textContent = ` Computer wins, You lose!`;
+    playerScore = 0;
+    computerScore = 0;
   }
-  console.log(`Your score : ${playerScore}`);
-  console.log(`Computer score : ${computerScore}`);
-  let winner =
-    playerScore > computerScore
-      ? `You win!`
-      : playerScore === computerScore
-      ? `Tied Game!`
-      : `You lose!`;
-  console.log(winner);
 }
-playGame();
